de3b5c6a76ac204a10ebcb84e86d1f04654777a0
sumit-TGT <sumit@recibo.site>
2025-08-02 22:19:00 +0530

    Add files via upload

---
+++ receipt.controller.js
@@ -0,0 +1,50 @@
+const Receipt = require('../models/receipt.model');
+const syncQuickBooks = require('../services/syncQuickBooks');
+const syncZoho = require('../services/syncZoho');
+const syncTally = require('../services/syncTally');
+
+exports.createReceipt = async (req, res, next) => {
+  try {
+    const receipt = await Receipt.create(req.body);
+    res.status(201).json(receipt);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getReceipt = async (req, res, next) => {
+  try {
+    const receipt = await Receipt.findById(req.params.id).lean();
+    if (!receipt) return res.status(404).json({ message: 'Receipt not found' });
+    res.json(receipt);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.syncReceipt = async (req, res, next) => {
+  try {
+    const receipt = await Receipt.findById(req.params.id).lean();
+    if (!receipt) return res.status(404).json({ message: 'Receipt not found' });
+
+    let result;
+    switch (receipt.platform) {
+      case 'quickbooks':
+        result = await syncQuickBooks(receipt);
+        break;
+      case 'zoho':
+        result = await syncZoho(receipt);
+        break;
+      case 'tally':
+        result = await syncTally(receipt);
+        break;
+      default:
+        return res.status(400).json({ message: 'Unsupported platform' });
+    }
+
+    res.json({ message: 'Sync successful', result });
+  } catch (err) {
+    next(err);
+  }
+};
+
